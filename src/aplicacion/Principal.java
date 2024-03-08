0b8102249441ae5b30acfd949c1574303647a8c0
claraalvaarez <145555928+claraalvaarez@users.noreply.github.com>
2024-03-08 09:26:44 +0100

    Update Principal.java

---
+++ src/aplicacion/Principal.java
@@ -0,0 +1,17 @@
+package aplicacion;
+import mates.Matematicas;
+/**
+ * La clase {@code Aplicacion} proporciona un programa principal para calcular una aproximación del número Pi
+ * utilizando el método de Monte Carlo de forma recursiva.
+ */
+public class Principal {
+    /**
+     * El método principal para ejecutar el programa.
+     *
+     * @param args los argumentos de la línea de comandos. Se espera que el primer argumento sea el número de pasos.
+     */
+    public static void main(String[] args) {
+        // Imprimir el resultado de la aproximación de Pi
+        System.out.println("El número PI es " + Matematicas.generarNumeroPiRecursivo(Integer.parseInt(args[0])));
+    }
+}
